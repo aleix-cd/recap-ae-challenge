fa91744bf4f631f3ea0531250c6a31917884653f
Aleix Cubel Capdevila <aleix.cubel@gmail.com>
2025-12-11 07:20:31 +0100

    clean up create_source_tables.py

--- duckdb/create_source_tables.py
+++ duckdb/create_source_tables.py
@@ -17,11 +17,10 @@ con.execute(f"""
     select * from read_csv_auto('{INVOICE_TRANSACTION_MATCHES_CSV}');
 """)
 
-print("Invoices preview:")
+print("Invoices table preview:")
 con.sql("select * from invoices limit 5").show()
 
-print("\nInvoice-Transaction Matches preview:")
+print("\nInvoice-Transaction Matches table preview:")
 con.sql("select * from invoice_transaction_matches limit 5").show()
 
-# Close connection
 con.close()
